121b08612ad7d9346f6132f53e6d7235c8029ace
abhishek <162462241+abhishekkirole@users.noreply.github.com>
2026-02-23 23:21:18 +0530

    Update solution.java

--- 1. Pattern: Two Pointers/Pair with Target Sum /solution.java
+++ 1. Pattern: Two Pointers/Pair with Target Sum /solution.java
@@ -1,1 +1,26 @@
+import java.util.*;
 
+class Solution {
+    public int[] pairWithTargetSum(int[] nums, int target) {
+        Arrays.sort(nums);   // Sort the array first
+        
+        int left = 0;
+        int right = nums.length - 1;
+        
+        while (left < right) {
+            int sum = nums[left] + nums[right];
+            
+            if (sum == target) {
+                return new int[]{nums[left], nums[right]};
+            }
+            else if (sum < target) {
+                left++;     // Need bigger sum
+            }
+            else {
+                right--;    // Need smaller sum
+            }
+        }
+        
+        return new int[]{-1, -1};  // No pair found
+    }
+}
